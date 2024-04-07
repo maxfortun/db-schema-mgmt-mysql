673682ae1d5bfbd6d308a5448c7513f011880e48
Max <max@maxf.net>
2024-04-07 11:11:32 -0400

    updated

--- example/setenv-mysql.sh
+++ example/setenv-mysql.sh
@@ -2,5 +2,13 @@ SWD=$( cd $( dirname "${BASH_SOURCE[0]}" ); pwd )
 
 MYSQL_HOSTNAME=$(cat $SWD/.mysqld.host 2>/dev/null || true)
 MYSQL_USER=testuser
-MYSQL_PASSWORD=testpass
+
+if [ -f $SWD/.mysqld.user-password ]; then
+	MYSQL_PASSWORD=$(cat $SWD/.mysqld.user-password)
+fi
+
+if [ -z $MYSQL_PASSWORD ]; then
+	MYSQL_PASSWORD=$(openssl rand -hex 16 | tee $SWD/.mysqld.user-password)
+fi
+
 MYSQL_DATABASE=testdb
